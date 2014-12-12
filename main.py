1362c191b65d5768faf6397624b339f25d4ee6fd
SSARCandy <ssarcandy@gmail.com>
2014-12-13 00:24:36 +0800

    [AUTO HEROKU DEPLOY]

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ Author: Colin Su <littleq0903@gmail.com>
 from bottle import Bottle, run, template
 from bottle import static_file
 from bottle import jinja2_template
+from bottle import request
+from bottle import static_file
+import json
 import facebook
 
 import os
+
+
 
 # setup these environment variable by adding "heroku.config"
 # - FACEBOOK_APP_ID
@@ -20,10 +25,30 @@ FACEBOOK_SECRET = os.environ['FACEBOOK_SECRET']
 app = Bottle()
 
 # Checkpoint 0
-@app.route('/login')
-def login():
+@app.route('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root='static/')
+
+@app.route('/main_map')
+def main_map():
+    template_data = {
+        'facebook_app_id': FACEBOOK_APP_ID
+    }
+    return static_file('main_map.html', root='./')
+	
+@app.route('/index')
+def index():
     template_data = {
         'facebook_app_id': FACEBOOK_APP_ID
     }
     return jinja2_template('index.html', **template_data)
+
+@app.route('/cp1')
+def cp1():
+    fb_access_token = request.cookies['fb_access_token']
+    fb_graph = facebook.GraphAPI(fb_access_token)
+    profile = fb_graph.get_object("me")
+    return json.dumps(profile)
+        
+    #    return "cp1"
 

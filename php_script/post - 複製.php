7218d3dafc214ce4503c9ad4795c721a57a52436
SSARCandy <ssarcandy@gmail.com>
2014-12-28 23:14:36 +0800

    remove trash

--- php_script/post - 複製.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include("../include/connect.php");
-	$tmp_file_name = $_FILES['Filedata']['tmp_name'];
-	$ok = move_uploaded_file($tmp_file_name, './images/database/'.$tmp_file_name);
-
-	// This message will be passed to 'oncomplete' function
-	echo $ok ? "OK" : "FAIL";
-?>

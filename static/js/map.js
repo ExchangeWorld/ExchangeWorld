8c0bd0fd39b6ba9cb00d56e11562a690f04718c4
Xhriman <seal789ie@gmail.com>
2015-01-04 10:44:45 +0800

    marker css

--- static/js/map.js
+++ static/js/map.js
@@ -7,6 +7,8 @@ var infowindow;
 var map;
 
 $(document).ready(function () {
+
+    /*----------------Input Clear Button-----------------*/
     $(".autocomplete").keyup(function () {
         $(this).next().toggle(Boolean($(this).val()));
     });
@@ -15,6 +17,7 @@ $(document).ready(function () {
         $(this).prev().val('').focus();
         $(this).hide();
     });
+    /*-------------------------------------------------*/
 });
 
 function initialize() {
@@ -23,7 +26,8 @@ function initialize() {
     var mapProp = {
         //center: new google.maps.LatLng(24.9853919, 121.5865058),
         zoom: 17,
-        //draggableCursor: 'url(http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/glossy-black-icons-business/080745-glossy-black-icon-business-cursor.png), auto;',
+        draggableCursor: 'crosshair',
+
         disableDoubleClickZoom: false,
         panControl: false,
         mapMaker:false,
@@ -38,6 +42,7 @@ function initialize() {
         mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    //map.setOptions({draggableCursor: default});
 
     // Try W3C Geolocation (Preferred)
     if (navigator.geolocation) {
@@ -106,9 +111,12 @@ function addMarker(location) {
         flat: false,
         anchor: RichMarkerPosition.MIDDLE,
         zIndex: 168,
-        content: '<div class="custom-marker">'+
+        content: '<div class="custom-marker normal-item">'+
           '<span><img class="contained-image" src="images/database/monkey.jpg"/></span>' +
           '</div>'
+    //    '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="480px" height="480px" viewBox="0 0 480 480" style="enable-background:new 0 0 480 480;" xml:space="preserve">'+
+	//'<path style="fill-rule:evenodd;clip-rule:evenodd;" d="M240,159.909c-44.235,0-80.091,35.859-80.091,80.091s35.855,80.091,80.091,80.091c44.231,0,80.091-35.859,80.091-80.091S284.231,159.909,240,159.909z M400,226.667h-25.694c-6.267-63.891-57.086-114.701-120.973-120.967V80c0-7.363-5.97-13.333-13.333-13.333S226.667,72.637,226.667,80v25.701c-63.889,6.266-114.705,57.075-120.971,120.966H80c-7.363,0-13.333,5.97-13.333,13.333s5.97,13.333,13.333,13.333h25.696c6.266,63.891,57.082,114.7,120.971,120.966V400c0,7.363,5.97,13.333,13.333,13.333s13.333-5.97,13.333-13.333v-25.701c63.888-6.266,114.707-57.075,120.974-120.966H400c7.363,0,13.333-5.97,13.333-13.333S407.363,226.667,400,226.667z M240,347.669c-59.463,0-107.666-48.209-107.666-107.669S180.537,132.331,240,132.331c59.466,0,107.669,48.209,107.669,107.669S299.466,347.669,240,347.669z"/>'+'</svg>'
+    
     });
 
     //return marker.getPosition();

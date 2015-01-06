206d6f7cb1fadeb8716890098401d1eba9fe71b8
Xhriman <seal789ie@gmail.com>
2015-01-06 21:57:46 +0800

    [update] map zoom-in and panTo animation

--- static/js/map.js
+++ static/js/map.js
@@ -26,7 +26,7 @@ function initialize() {
         center: new google.maps.LatLng(24.9853919, 121.5865058),
         zoom: 17,
         draggableCursor: 'default',
-        draggingCursor: 'crosshair',
+        draggingCursor: 'default',
         disableDoubleClickZoom: false,
 
         panControl: false,
@@ -96,23 +96,6 @@ function naigvator() {
         handleNoGeolocation(browserSupportFlag);
     }
 }
-
-//function postMarker(location) {
-//    if (map == null)
-//        return;
-
-//    marker = new RichMarker({
-//        position: location,
-//        map: map,
-//        draggable: true,
-//        flat: true,
-//        anchor: RichMarkerPosition.MIDDLE,
-//        zIndex: 10,
-//        content: '<div class="custom-marker normal-item">'+
-//          '<span><img class="contained-image" src="images/database/monkey.jpg"/></span>' +
-//          '</div>'
-//    });
-//};
 
 function postMarker(location, img) {
     if (map == null)
@@ -196,44 +179,44 @@ function addMarkers(lat, lng, img, gid) {
                     gid: val
                 },
                 success: function (response) {
-                //Left Side
-                $('#leftSideSwitch').hide().empty();
-                $('#leftSideSwitch').html('<div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px"><div class="col-md-3"><button id="goback" type="button" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Go back</button></div></div><div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px"><div class="col-md-5"><img src="' + response["photoPath"] + '" class="img-thumbnail" alt="..."></div><div class="col-md-7"><ul class="list-group" style="font-size: 70%"><li class="list-group-item">' + response["gname"] + '<span class="badge">' + response["categories"] + '</span> </li> <li class="list-group-item">Wanted : ' + response["want"] + '</li><li class="list-group-item owner" data-value="' + response["ownerID"] + '"><img src="' + response["owner_photo"] + '" height="20" width="20"> ' + response["username"] + '</li></ul></div></div><div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px; font-size: 85%"><div class="col-md-12"><div class="panel panel-default"><div class="panel-heading">Description : </div><div class="panel-body"><p>' + response["description"] + '</div></div></div></div><div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px; font-size: 70%">').show('fast');
+                    //Left Side
+                    $('#leftSideSwitch').hide().empty();
+                    $('#leftSideSwitch').html('<div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px"><div class="col-md-3"><button id="goback" type="button" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Go back</button></div></div><div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px"><div class="col-md-5"><img src="' + response["photoPath"] + '" class="img-thumbnail" alt="..."></div><div class="col-md-7"><ul class="list-group" style="font-size: 70%"><li class="list-group-item">' + response["gname"] + '<span class="badge">' + response["categories"] + '</span> </li> <li class="list-group-item">Wanted : ' + response["want"] + '</li><li class="list-group-item owner" data-value="' + response["ownerID"] + '"><img src="' + response["owner_photo"] + '" height="20" width="20"> ' + response["username"] + '</li></ul></div></div><div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px; font-size: 85%"><div class="col-md-12"><div class="panel panel-default"><div class="panel-heading">Description : </div><div class="panel-body"><p>' + response["description"] + '</div></div></div></div><div class="row" style="background-color: silver; padding-top: 0px; margin-top: 15px; font-size: 70%">').show('fast');
 
-                //Map Side
-                map.setCenter(new google.maps.LatLng(response["posY"], response["posX"]));
-                map.setZoom(17);
-            },
-            error: function (xhr, ajaxOption, thrownError) {
-                alert(thrownError);
-                alert(JSON.stringify(xhr));
-            }
-        });
-
-        //Handle Random Recommand Tables
-        $.ajax({
-            type: "GET",
-            url: "./php_script/tables.php",
-            dataType: "json",
-            data: {
-                type: "recommand",
-                uid : val
-            },
-            success: function (response) {
-                $('#leftSideSwitch').append('<div class="col-md-12" style="padding: 0px"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">You might also check ...</h3></div><div class="panel-body" id="recommandTables"></div></div></div></div>');
-                for (var i = 0; i < 4; i++) {
-                    var min = 0;
-                    var max = response.length-1;
-                    var xx  = Math.floor(Math.random()*(max-min+1)+min);
-                    //alert(xx);
-                    $('#recommandTables').append('<div class="col-md-3 searchResult" style="padding: 0px; padding-top: 0px; padding-bottom: 0px; border: 0px; background: #fff; margin: 0px;" data-value="' + response[xx]["gid"] + '"><img src="' + response[xx]["photoPath"] + '" width="50" class="img-thumbnail" alt="..."></div>');
+                    //Map Side
+                    map.panTo(new google.maps.LatLng(response["posY"], response["posX"]));
+                    map.setZoom(17);
+                },
+                error: function (xhr, ajaxOption, thrownError) {
+                    alert(thrownError);
+                    alert(JSON.stringify(xhr));
                 }
-            },
-            error: function (xhr, ajaxOption, thrownError) {
-                alert(thrownError);
-                alert(JSON.stringify(xhr));
-            }
-        });
+            });
+
+            //Handle Random Recommand Tables
+            $.ajax({
+                type: "GET",
+                url: "./php_script/tables.php",
+                dataType: "json",
+                data: {
+                    type: "recommand",
+                    uid: val
+                },
+                success: function (response) {
+                    $('#leftSideSwitch').append('<div class="col-md-12" style="padding: 0px"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">You might also check ...</h3></div><div class="panel-body" id="recommandTables"></div></div></div></div>');
+                    for (var i = 0; i < 4; i++) {
+                        var min = 0;
+                        var max = response.length - 1;
+                        var xx = Math.floor(Math.random() * (max - min + 1) + min);
+                        //alert(xx);
+                        $('#recommandTables').append('<div class="col-md-3 searchResult" style="padding: 0px; padding-top: 0px; padding-bottom: 0px; border: 0px; background: #fff; margin: 0px;" data-value="' + response[xx]["gid"] + '"><img src="' + response[xx]["photoPath"] + '" width="50" class="img-thumbnail" alt="..."></div>');
+                    }
+                },
+                error: function (xhr, ajaxOption, thrownError) {
+                    alert(thrownError);
+                    alert(JSON.stringify(xhr));
+                }
+            });
         }
 
     });
@@ -269,8 +252,8 @@ function markersBounds() {
             topBound = y;
     }
 
-    var sw = new google.maps.LatLng(bottomBound - 0.1, leftBound - 0.1);
-    var ne = new google.maps.LatLng(topBound + 0.1, rightBound + 0.1);
+    var sw = new google.maps.LatLng(bottomBound - 0.0006, leftBound - 0.0006);
+    var ne = new google.maps.LatLng(topBound + 0.0006, rightBound + 0.0006);
 
     //map.panToBounds(new google.maps.LatLngBounds(sw, ne));
     map.fitBounds(new google.maps.LatLngBounds(sw, ne));

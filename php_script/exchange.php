ceced89f6ec2739d27ce64d77c8355eeab8fec05
SSARCandy <ssarcandy@gmail.com>
2015-01-06 11:05:58 +0800

    [update] minor change
    +  categories  show as string
    +  show Owner at Seek

--- php_script/exchange.php
+++ php_script/exchange.php
@@ -7,6 +7,11 @@
 	$result = mysql_query($sql);	
 	//while ($row = mysql_fetch_array($result)) {	
 	$row = mysql_fetch_array($result);
+	if($row["categories"]==1) $row["categories"] = "Antiques";
+	if($row["categories"]==2) $row["categories"] = "Art";
+	if($row["categories"]==3) $row["categories"] = "Book";
+	if($row["categories"]==4) $row["categories"] = "Clothing";
+
 	//	$rows[] = $row;
 	//}
 	echo json_encode($row);

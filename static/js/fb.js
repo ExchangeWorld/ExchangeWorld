a0a8531c2fa4610b12dbd370b3060443b247cd47
SSARCandy <ssarcandy@gmail.com>
2015-04-01 19:09:04 +0800

    [update]

--- static/js/fb.js
+++ static/js/fb.js
@@ -47,7 +47,7 @@ function fbLoaded() {
                 var usr_name = response.name;
                 FB.api('/me/picture?width=250', function (response) {
                     my_picture_url = response.data.url;
-                    $("#myname").html('<img id="myhead" src="'+my_picture_url+'" height="20" width="20">'+usr_name);
+                    $("#myname").html('<img id="myhead" src="'+my_picture_url+'" height="25" width="25">'+"  "+usr_name);
                 });
             });
 

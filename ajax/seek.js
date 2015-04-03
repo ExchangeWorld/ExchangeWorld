97ce39de05bed72499df94435a24635290101552
SSARCandy <ssarcandy@gmail.com>
2015-04-04 02:19:12 +0800

    complete multi-keyword search

--- ajax/seek.js
+++ ajax/seek.js
@@ -104,21 +104,23 @@ function seek_query(keywords, area, cate)
                 }
                 else
                 {
+                    var keyword_strings;
+                    if(keywords != "") keyword_strings = keywords.split(" ");
                     for (var i = 0; i < response.length; i++)
                     {
                         // Highlight Keywords in results
                         var gname = response[i]["gname"];
                         if(keywords != "")
                         {
-                            var keyword_strings = keywords.split(" ");
-                            for(var i=0; i<keyword_strings.length; i++)
+                            //var keyword_strings = keywords.split(" ");
+                            for(var j=0; j<keyword_strings.length; j++)
                             {
-                                if(keyword_strings[i]==="") continue;
-                                keyword_strings[i] = keyword_strings[i].replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
-                                var pattern = new RegExp("("+keyword_strings[i]+")", "gi");
+                                if(keyword_strings[j]==="") continue;
+                                keyword_strings[j] = keyword_strings[j].replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
+                                var pattern = new RegExp("("+keyword_strings[j]+")", "gi");
 
                                 gname = gname.replace(pattern, "<mark>$1</mark>");
-                                gname = gname.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/,"$1</mark>$2<mark>$4");
+                               // gname = gname.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/,"$1</mark>$2<mark>$4");
                             }
                         }
 

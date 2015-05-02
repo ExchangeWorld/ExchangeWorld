5838c2b6d31405178055266080c85d31515421ec
xhriman <seal789ie@gmail.com>
2015-05-02 18:34:20 +0800

    update
    npm
    + bludbird
    + express
    + spdy
    bower update
    - old website and static files
    rename: css --> styles
    _src --> static

--- php_script/upload_photo.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include("./connect.php");
-	$tmp_file_name = $_FILES['fileToUpload'];
-	$ok = move_uploaded_file($tmp_file_name, './images/database/'.$tmp_file_name);
-
-	// This message will be passed to 'oncomplete' function
-	echo $ok ? "OK" : "FAIL";
-?>

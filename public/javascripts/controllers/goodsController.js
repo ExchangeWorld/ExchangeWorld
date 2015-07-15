91fe654aa3a4d0d0fb9474ab9ceac38becd76981
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-07-15 22:44:55 +0800

    bug for goods click event

--- public/javascripts/controllers/goodsController.js
+++ public/javascripts/controllers/goodsController.js
@@ -1,11 +1,12 @@
 (function() {
     var goodsController = angular.module('goodsController', ['goodsServices']);
 
-    goodsController.controller('goodsCtrl', ['$scope', 'goodsServ', function($scope, goodsServ) {
+    goodsController.controller('goodsCtrl', ['$scope', 'goodsServ', '$routeParams',
+    function($scope, goodsServ, $routeParams) {
         goodsServ.get(function(data) {
             //$scope= data;
             $scope.goodsProps = data;
-        });
+        }, $routeParams.gid);
 
         $scope.test = 5;
 
--- public/javascripts/services/goodsServices.js
+++ public/javascripts/services/goodsServices.js
@@ -1,7 +1,7 @@
 angular.module('goodsServices', []).factory('goodsServ', ['$http', function($http) {
     return {
-        get: function(callback) {
-            $http.get('/goods?gid=38').success(function(data) {
+        get: function(callback, id) {
+            $http.get('/goods?gid=' + id).success(function(data) {
                 // prepare data here
                 console.log(data);
                 callback(data);

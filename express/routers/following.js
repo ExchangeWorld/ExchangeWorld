b8398f98e28bbc757e2e30ee8fd6e40d529deb41
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-08-10 00:55:34 +0800

    feature(follow/api) : new api

---
+++ express/routers/following.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router  = express.Router();
+
+// including tables 
+var following = require('../ORM/Following');
+
+router.get('/', function(req, res, nex) {
+
+	// Available params:
+	// 
+	// fb_id 
+	// 
+
+	var _fb_id = req.query.fb_id;
+
+
+	// Emit a find operation with orm model in table `following`
+	following
+		.sync({force: false})
+		.then(function() {
+
+			return following.findAll({
+				where: {
+					myid : _fb_id
+				},
+			});
+		})
+		.then(function(result) {
+			res.json(result);
+		});
+});
+
+
+module.exports = router;

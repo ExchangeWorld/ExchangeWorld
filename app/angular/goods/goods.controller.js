7fe978a8946f17d1d8437bc239c1819d486d1f4a
Xhriman <seal789ie@gmail.com>
2015-08-10 13:19:09 +0800

    minor change

--- app/angular/goods/goods.controller.js
+++ app/angular/goods/goods.controller.js
@@ -4,26 +4,10 @@ const goodsModule = require('./goods.module');
 goodsModule.controller('GoodsController', GoodsController);
 
 /** @ngInject */
-function GoodsController(goodData, goodsService, $stateParams, $state) {
+function GoodsController(goodData, $state) {
 	const vm       = this;
-	vm.goodsData   = goodData;
+	vm.goodData   = goodData;
 	vm.onClickUser = onClickUser;
-	//activate();
-
-	/////////////
-
-	//function activate() {
-	//	// get data from goodsServices.js
-	//	console.log(goodData);
-	//	goodsService
-	//		.getGood($stateParams.gid)
-	//		.then(function(data) {
-	//			vm.goodsData = data;
-	//		})
-	//		//.catch(function(error){
-	//		//	console.log('error', error);
-	//		//});
-	//}
 
 	// define onClick event on goods owner
 	function onClickUser(fb_id) {

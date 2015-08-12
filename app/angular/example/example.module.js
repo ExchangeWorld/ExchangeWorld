3627cede710d4818b186ff2f68bb56ef7825ec9b
Xhriman <seal789ie@gmail.com>
2015-08-12 12:40:18 +0800

    delete example

--- app/angular/example/example.module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const angular = require('angular');
-const bulk    = require('bulk-require');
-
-require('../map/map.module');
-module.exports = angular.module('app.example', ['app.core']);
-
-bulk(__dirname, ['./**/!(*.module|*.spec).js']);

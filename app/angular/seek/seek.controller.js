aa1ff3680484ad00bf8eb2d7c2a496fb466b1141
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-08-13 04:33:50 +0800

    refactor(seek/frontend): search

--- app/angular/seek/seek.controller.js
+++ app/angular/seek/seek.controller.js
@@ -11,34 +11,18 @@ function SeekController(seekService, $state, AvailableCategory) {
 	vm.searchGoodsCategory = '';
 	vm.onClickGoods        = onClickGoods;
 	vm.onSearch            = onSearch;
-	vm.availableCategory = AvailableCategory;
+	vm.availableCategory   = AvailableCategory;
 
-	activate();
+	onSearch();
 
 	/////////
-
-	function activate() {
-		seekService
-			.getSeek({})
-			.then(function(data) {
-				vm.goods = data;
-			})
-			.catch(function() {
-				vm.goods = undefined;
-			});
-	}
-
-	//goods onClick event: change route to corrsponding gid
-	function onClickGoods(_gid) {
-		$state.go('root.withSidenav.goods', { gid: _gid });
-	}
 
 	function onSearch(){
 		var constrain = {
 			name     : vm.searchGoodsName,
-			category : vm.searchGoodsCategory,
+			category : vm.searchGoodsCategory.label,
 		};
-		console.log(constrain);
+		//console.log(constrain);
 
 		seekService
 			.getSeek(constrain)
@@ -50,4 +34,11 @@ function SeekController(seekService, $state, AvailableCategory) {
 				vm.goods = undefined;
 			});
 	}
+	
+
+	//goods onClick event: change route to corrsponding gid
+	function onClickGoods(_gid) {
+		$state.go('root.withSidenav.goods', { gid: _gid });
+	}
+
 }

e3cb70e6296f0dd4efa21bb799ea4579dc89e2e2
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-08-12 21:18:11 +0800

    fix(profile/front-end) : correspond change for profile

--- app/angular/post/post.controller.js
+++ app/angular/post/post.controller.js
@@ -21,19 +21,27 @@ function PostController(postService, $state) {
 
 	function onSubmit() {
 		var newPost = {
-			gname       : vm.goodsName,
+			name        : vm.goodsName,
 			description : vm.goodsDescriptions,
-			categories  : vm.goodsCategory.label,
-			want        : '',
-			posX        : 123.4,
-			posY        : 23.5,
-			ownerID     : '88776654',
+			category    : vm.goodsCategory.label,
+			position_x  : 123.4,
+			position_y  : 23.4,
+			owner_uid   : '88776654',
 			photo_path  : '',
 		};
 		
 
-		newPost.photo_path = postService.uploadImg(vm.imgEncoded);
-		postService.sendNewPostInfo(newPost);
+		/**
+		 * First, upload the photo and get photo_path,
+		 * then send new post data to backend
+		 */
+		postService
+			.uploadImg(vm.imgEncoded)
+			.then(function(data){
+				newPost.photo_path = data;
+
+				postService.sendNewPostInfo(newPost);
+			});
 
 		$state.go('root.withSidenav.seek');
 	}
--- app/angular/post/post.service.js
+++ app/angular/post/post.service.js
@@ -19,6 +19,18 @@ function postService(Restangular, $q) {
 	}
 
 	function uploadImg(img){
-		return Restangular.all('/upload/image').post(img);
+		const defer = $q.defer();
+
+		Restangular.all('/upload/image').post(img)
+			.then(function(data) {
+				if (data !== undefined ) {
+					defer.resolve(data);
+				} 
+			})
+			.catch(function(error) {
+				return exception.catcher('[Post Service] uploadImg error: ')(error);
+			});
+
+		return defer.promise;
 	}
 }

ffbd8101774e2a3d97326ca3604baf6e082a3563
LonSilent <101703044@nccu.edu.tw>
2015-08-13 22:37:35 +0800

    test progress circle,post tips

--- app/angular/seek/seek.controller.js
+++ app/angular/seek/seek.controller.js
@@ -4,29 +4,35 @@ const seekModule = require('./seek.module');
 seekModule.controller('SeekController', SeekController);
 
 /** @ngInject */
-function SeekController($scope, seekService, $state) {
-	var vm          = this;
-	vm.goods        = [];
-	vm.onClickGoods = onClickGoods;
+function SeekController($scope, seekService, $state, $timeout) {
+    var vm = this;
+    vm.goods = [];
+    vm.onClickGoods = onClickGoods;
 
-	activate();
+    $scope.loading = false;
+    activate();
+    $timeout(function() {
+        $scope.loading = false;
+    }, 1500);
+    $scope.loading = true;
+    /////////////
 
-	/////////////
+    function activate() {
+        // Use seek.service.js to get data from backend
+        seekService.getSeekData()
+            .then(function(data) {
+                // promise fulfilled, collect all goods datas
+                vm.goods = data;
+            }).catch(function(error) {
+                // promise rejected 
+                console.log('error', error);
+            });
+    }
 
-	function activate() {
-		// Use seek.service.js to get data from backend
-		seekService.getSeekData()
-			.then(function(data) {
-				// promise fulfilled, collect all goods datas
-				vm.goods = data;
-			}).catch(function(error) {
-				// promise rejected 
-				console.log('error', error);
-			});
-	}
-
-	//goods onClick event: change route to corrsponding gid
-	function onClickGoods(gid) {
-		$state.go('root.goods', {gid:gid});
-	}
+    //goods onClick event: change route to corrsponding gid
+    function onClickGoods(gid) {
+        $state.go('root.goods', {
+            gid: gid
+        });
+    }
 }

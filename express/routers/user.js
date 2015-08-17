0754a2079b94e9d6bb016662ddb60269858c1140
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-08-17 15:39:31 +0800

    fix(user): email constrain when register

--- express/routers/user.js
+++ express/routers/user.js
@@ -64,7 +64,7 @@ router.post('/register', function(req, res, next) {
 
 	var _fb_id     = req.body.fb_id;
 	var _name      = req.body.name;
-	var _email     = req.body.email;
+	var _email     = req.body.email || '';
 	var _photo_path = req.body.photo_path;
 
 	// Create instance
@@ -72,10 +72,10 @@ router.post('/register', function(req, res, next) {
 		.sync({force: false})
 		.then(function() {
 			return users.create({
-				fb_id:     _fb_id,
-				name:      _name,
-				email:     _email,
-				photo_path: _photo_path
+				fb_id      : _fb_id,
+				name       : _name,
+				email      : _email,
+				photo_path : _photo_path
 			});
 		})
 		.then(function(result) {

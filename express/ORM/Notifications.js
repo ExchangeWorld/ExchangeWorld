0d924a6f5299d8b7af0309106d245ce21983eba2
NoelBao <noeleon930@gmail.com>
2015-08-17 15:15:00 +0800

    feature(Notifications / backend): Set schema of Notifications (with timestamp)

--- express/ORM/Notifications.js
+++ express/ORM/Notifications.js
@@ -2,7 +2,30 @@ var Sequelize = require('sequelize');
 var sequelize = require('../libs/orm');
 
 // Define the schema of table `notifications`
-
-var Notifications = sequelize.define('notifications', {});
+var Notifications = sequelize.define('notifications', {
+	nid: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		allowNull: false,
+		unique: true,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	receiver_uid: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		allowNull: false
+	},
+	trigger_reason: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+	content: {
+		type: Sequelize.TEXT,
+		allowNull: false
+	}
+}, {
+	timestamp: true,
+	createdAt: 'timestamp',
+	updatedAt: false,
+});
 
 module.exports = Notifications;

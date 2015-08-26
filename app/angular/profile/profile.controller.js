cb75dfe0ea4acb1f5e62c8cd96df32f2eb45a9ab
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-08-26 15:14:49 +0800

    feature(profile): add following

--- app/angular/profile/profile.controller.js
+++ app/angular/profile/profile.controller.js
@@ -1,23 +1,33 @@
 'use strict';
 
 const profileModule = require('./profile.module');
+const _             = require('lodash');
 profileModule.controller('ProfileController', ProfileController);
 
 /** @ngInject */
-function ProfileController(profile, $state, $localStorage, auth) {
-	var vm           = this;
-	const types      = ['following', 'follower'];
-	vm.profile       = profile;
-	vm.largePic      = '';
-	vm.isLoggedIn    = $localStorage.user ? true : false;
-	vm.isMe          = vm.isLoggedIn ? (profile.uid === $localStorage.user.uid) : false;
-	vm.onClickFollow = onClickFollow;
-
+function ProfileController(profile, profileService, $state, $localStorage, auth) {
+	var vm                 = this;
+	const types            = ['following', 'follower'];
+	vm.profile             = profile;
+	vm.largePic            = '';
+	vm.isLoggedIn          = $localStorage.user ? true : false;
+	vm.isMe                = vm.isLoggedIn ? (profile.uid === $localStorage.user.uid) : false;
+	vm.onClickFollow       = onClickFollow;
+	vm.onClickAddFollowing = onClickAddFollowing;
+	vm.followerCount       = profile.followers.length;
+	vm.isFollowed          = false;
 
 	/////////////
 	activate();
 
-	function activate(){
+	function activate() {
+		if(vm.isLoggedIn) {
+			var idx = _.findIndex(profile.followers, function(user) { 
+				return user.follower_uid === $localStorage.user.uid; 
+			});
+			if(idx !== -1) vm.isFollowed = true;
+		}
+
 		auth
 			.getLoginState()
 			.then(function(data) {
@@ -35,5 +45,11 @@ function ProfileController(profile, $state, $localStorage, auth) {
 			uid: uid,
 			type: types[idx]
 		});
+	}
+
+	function onClickAddFollowing() {
+		profileService.addFollowing($localStorage.user.uid, profile.uid);
+		vm.followerCount++;
+		vm.isFollowed = !vm.isFollowed;
 	}
 }

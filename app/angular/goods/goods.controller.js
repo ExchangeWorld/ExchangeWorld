7cd7df488bd2451884e6cdeba6c9effa238dcfd1
Xhriman <seal789ie@gmail.com>
2015-08-27 10:16:31 +0800

    timeout mapMoveTo

--- app/angular/goods/goods.controller.js
+++ app/angular/goods/goods.controller.js
@@ -4,14 +4,16 @@ const goodsModule = require('./goods.module');
 goodsModule.controller('GoodsController', GoodsController);
 
 /** @ngInject */
-function GoodsController(goodData, $state, $scope) {
+function GoodsController(goodData, $state, $scope, $timeout) {
 	const vm       = this;
 	vm.goodData    = goodData;
 	vm.onClickUser = onClickUser;
 
 	function activate() {
 		$scope.$parent.$broadcast('goodsChanged', [goodData]);
-		$scope.$parent.$broadcast('mapMoveTo', goodData.gid);
+		$timeout(function() {
+			$scope.$parent.$broadcast('mapMoveTo', goodData.gid);
+		}, 50);
 	}
 
 	// define onClick event on goods owner

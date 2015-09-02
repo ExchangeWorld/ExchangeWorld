0595f3e17dca9cf9ba761ee00699f45b2fecf265
LonSilent <101703044@nccu.edu.tw>
2015-09-02 21:30:13 +0800

    updates

--- app/angular/core/core-theme.config.js
+++ app/angular/core/core-theme.config.js
@@ -10,7 +10,7 @@ function materialTheme($mdThemingProvider) {
 			'default': '600', // by default use shade 600 from the teal palette for primary intentions
 			'hue-1': '400', // use shade 400 for the <code>md-hue-1</code> class
 			'hue-2': '900', // use shade 900 for the <code>md-hue-2</code> class
-			'hue-3': '100' // use shade A100 for the <code>md-hue-3</code> class
+			'hue-3': '50' // use shade A100 for the <code>md-hue-3</code> class
 		})
 		// If you specify less than all of the keys, it will inherit from the
 		// default shades

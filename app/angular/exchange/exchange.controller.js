355dd2651cf6268bafe8aa064bc7cb6f99a021f8
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-09-09 15:08:41 +0800

    integrate exchange with controller

--- app/angular/exchange/exchange.controller.js
+++ app/angular/exchange/exchange.controller.js
@@ -4,7 +4,7 @@ const exchangeModule = require('./exchange.module');
 exchangeModule.controller('ExchangeController', ExchangeController);
 
 /** @ngInject */
-function ExchangeController(exchangeList, $state, exchangeService) {
+function ExchangeController(exchangeList, $state, exchangeService, $stateParams) {
 	var vm             = this;
 	vm.exchangeList    = exchangeList;
 	vm.exchange        = {};
@@ -16,7 +16,18 @@ function ExchangeController(exchangeList, $state, exchangeService) {
 	activate();
 
 	function activate() {
+			console.log(vm.exchangeList);
 		if(vm.exchangeList.length) {
+			vm.exchangeList.forEach(function(exchange) {
+				exchangeService
+					.getExchange(exchange.eid)
+					.then(function(data) {
+						//console.log(data);
+						exchange.details = data;
+						exchange.with = (data.goods[0].owner_uid === $stateParams.uid) ? data.goods[0].user.name : data.goods[1].user.name ;
+					});
+			});
+			console.log(vm.exchangeList);
 			onClickExchange(vm.exchangeList[0].eid);
 		}
 	}

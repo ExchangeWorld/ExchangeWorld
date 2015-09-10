f30e7d1d87d27741719e672903069d1ef4ee1e84
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-09-11 00:30:17 +0800

    refactor

--- app/angular/utils/photoSlider/photoSlider.directive.js
+++ app/angular/utils/photoSlider/photoSlider.directive.js
@@ -10,26 +10,26 @@ function photoSliderDirective() {
 	const directive = {
 		restrict: 'AE',
 		replace: true,
+		templateUrl: 'utils/photoSlider/photoSlider.html',
+		link: photoSliderPostLink,
 		scope: {
 			images: '='
 		},
-		link: function (scope, elem, attrs) {
-			scope.currentSlide   = 0;
-			scope.isCurrentSlide = function(idx) { return idx === scope.currentSlide; };
-			scope.prevSlide      = prevSlide;
-			scope.nextSlide      = nextSlide;
-
-			function prevSlide() {
-				scope.currentSlide = (scope.currentSlide < scope.images.length - 1) ? ++scope.currentSlide : 0;
-			}
-
-			function nextSlide() {
-				scope.currentSlide = (scope.currentSlide > 0) ? --scope.currentSlide : scope.images.length - 1;
-			}
-		},
-		templateUrl : 'utils/photoSlider/photoSlider.html',
 	};
-
 	return directive;
 }
 
+function photoSliderPostLink(scope, elem, attrs) {
+	scope.currentSlide   = 0;
+	scope.isCurrentSlide = function(idx) { return idx === scope.currentSlide; };
+	scope.prevSlide      = prevSlide;
+	scope.nextSlide      = nextSlide;
+
+	function prevSlide() {
+		scope.currentSlide = (scope.currentSlide < scope.images.length - 1) ? ++scope.currentSlide : 0;
+	}
+
+	function nextSlide() {
+		scope.currentSlide = (scope.currentSlide > 0) ? --scope.currentSlide : scope.images.length - 1;
+	}
+}

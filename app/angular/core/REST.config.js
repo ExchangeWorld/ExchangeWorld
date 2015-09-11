ab394d0d127d759a9e8bd461eeec69f71b7680de
LonSilent <101703044@nccu.edu.tw>
2015-09-11 14:51:27 +0800

    updates

--- app/angular/core/REST.config.js
+++ app/angular/core/REST.config.js
@@ -4,7 +4,7 @@ const coreModule = require('./core.module');
 coreModule.config(RestConfig);
 
 function RestConfig(RestangularProvider) {
-	RestangularProvider.setBaseUrl('http://140.119.164.169:3002/api');
+	RestangularProvider.setBaseUrl('http://192.168.43.56:3002/api');
 
 	// set params for multiple methods at once
 	// Restangular.setDefaultRequestParams(['remove', 'post'], {accessToken: "secret key"});

a06ca8d545a2ecf17955069894a9a1e2fab343d7
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-09-11 11:01:00 +0800

    prevent event propagation

--- app/angular/utils/photoSlider/photoSlider.directive.js
+++ app/angular/utils/photoSlider/photoSlider.directive.js
@@ -19,17 +19,22 @@ function photoSliderDirective() {
 	return directive;
 }
 
-function photoSliderPostLink(scope, elem, attrs) {
+function photoSliderPostLink(scope) {
+	//console.log(scope.images.length);
 	scope.currentSlide   = 0;
 	scope.isCurrentSlide = function(idx) { return idx === scope.currentSlide; };
 	scope.prevSlide      = prevSlide;
 	scope.nextSlide      = nextSlide;
 
-	function prevSlide() {
+	function prevSlide(e) {
 		scope.currentSlide = (scope.currentSlide < scope.images.length - 1) ? ++scope.currentSlide : 0;
+		e.preventDefault();
+		e.stopPropagation();
 	}
 
-	function nextSlide() {
+	function nextSlide(e) {
 		scope.currentSlide = (scope.currentSlide > 0) ? --scope.currentSlide : scope.images.length - 1;
+		e.preventDefault();
+		e.stopPropagation();
 	}
 }

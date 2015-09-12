fbcddd1252dfb74150d49c2722aa3da6b43af0c1
NoelBao <noeleon930@gmail.com>
2015-09-12 14:49:13 +0800

    fix(express/backend)

--- express/index.js
+++ express/index.js
@@ -26,7 +26,7 @@ module.exports = function() {
 	server.use(compression());
 	server.use(express.static(config.dist.root));
 
-	app.all('/', function(req, res, next) {
+	server.all('/', function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "X-Requested-With");
 		next();

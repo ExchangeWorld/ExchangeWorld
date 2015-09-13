93f6e346aa65c75cfe4e622a4227170e317cfeb7
NoelBao <noeleon930@gmail.com>
2015-09-14 06:10:10 +0800

    update(express/backend): adjust it to be a static file server

--- express/index.js
+++ express/index.js
@@ -2,6 +2,8 @@
 
 var config       = require('../gulp/config');
 var http         = require('http');
+var https        = require('https');
+var ssl          = require('../ssl/ssl');
 var express      = require('express');
 var gutil        = require('gulp-util');
 var morgan       = require('morgan');
@@ -19,19 +21,19 @@ module.exports = function() {
 	if (process.env.NODE_ENV !== 'production') server.use(morgan('dev'));
 
 	//server.use(favicon(__dirname + '/' + config.images.dest + '/favicon.ico'));
-	server.use(bodyParser.json({ limit : '64mb' })); // for parsing application/json
-	server.use(bodyParser.urlencoded({ limit : '64mb', extended : true })); // for parsing application/x-www-form-urlencoded
+	// server.use(bodyParser.json({ limit : '64mb' })); // for parsing application/json
+	// server.use(bodyParser.urlencoded({ limit : '64mb', extended : true })); // for parsing application/x-www-form-urlencoded
 	// server.use(multer()); // for parsing multipart/form-data
 	server.use(cookieParser());
 	server.use(compression());
 	server.use(express.static(config.dist.root));
 
-	server.all('*', function(req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		next();
-	});
+	// server.all('*', function(req, res, next) {
+	// 	res.header('Access-Control-Allow-Origin', '*');
+	// 	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	// 	next();
+	// });
 
 	// Serve index.html for all routes to leave routing up to Angular
 	server.all(/^(?!\/api\/)\w*/, function(req, res, next) {
@@ -45,33 +47,33 @@ module.exports = function() {
 	// If the path is not /api/authenticate, then it needs authentication
 	// If fail, return {"authentication": "fail"}
 	// If success, and then go next()
-	var authenticate = require('./routers/authenticate');
-	server.all(/\/api\/(?!authenticate).+/, authenticate.token, function(req, res, next) {
-		next();
-	});
+	// var authenticate = require('./routers/authenticate');
+	// server.all(/\/api\/(?!authenticate).+/, authenticate.token, function(req, res, next) {
+	// 	next();
+	// });
 
-	/* ref: doc/seek.md */
-	server.use('/api/goods/search', require('./routers/goods.search'));
-	server.use('/api/goods', require('./routers/goods'));
+	// /* ref: doc/seek.md */
+	// server.use('/api/goods/search', require('./routers/goods.search'));
+	// server.use('/api/goods', require('./routers/goods'));
 
-	server.use('/api/upload', require('./routers/upload'));
+	// server.use('/api/upload', require('./routers/upload'));
 
-	/* ref: doc/profile.md */
-	server.use('/api/user/profile/follower', require('./routers/follower'));
-	server.use('/api/user/profile/following', require('./routers/following'));
-	server.use('/api/user/profile', require('./routers/user.profile'));
-	server.use('/api/user', require('./routers/user'));
+	// /* ref: doc/profile.md */
+	// server.use('/api/user/profile/follower', require('./routers/follower'));
+	// server.use('/api/user/profile/following', require('./routers/following'));
+	// server.use('/api/user/profile', require('./routers/user.profile'));
+	// server.use('/api/user', require('./routers/user'));
 
-	server.use('/api/comment', require('./routers/comment'));
-	server.use('/api/star', require('./routers/star'));
-	server.use('/api/queue', require('./routers/queue'));
+	// server.use('/api/comment', require('./routers/comment'));
+	// server.use('/api/star', require('./routers/star'));
+	// server.use('/api/queue', require('./routers/queue'));
 
-	server.use('/api/exchange', require('./routers/exchange'));
-	server.use('/api/notification', require('./routers/notification'));
-	server.use('/api/message', require('./routers/message'));
-	server.use('/api/chatroom', require('./routers/chatroom'));
+	// server.use('/api/exchange', require('./routers/exchange'));
+	// server.use('/api/notification', require('./routers/notification'));
+	// server.use('/api/message', require('./routers/message'));
+	// server.use('/api/chatroom', require('./routers/chatroom'));
 
-	server.use('/api/authenticate', authenticate.router);
+	// server.use('/api/authenticate', authenticate.router);
 
 	// catch 404 and forward to error handler
 	server.use(function(req, res, next) {
@@ -107,4 +109,6 @@ module.exports = function() {
 
 	s.listen(config.serverPort);
 
+	var ss = https.createServer(ssl.options, server);
+	ss.listen(443)
 };

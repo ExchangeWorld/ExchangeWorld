8426a609ba2caaa67b2532a3453bd5f803d79dad
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-09-14 03:41:41 +0800

    constrain of post

--- app/angular/post/post.controller.js
+++ app/angular/post/post.controller.js
@@ -4,7 +4,7 @@ const postModule = require('./post.module');
 postModule.controller('PostController', PostController);
 
 /** @ngInject */
-function PostController(postService, $scope, $state, auth, AvailableCategory, logger, $localStorage) {
+function PostController(postService, $scope, $state, auth, AvailableCategory, logger, $localStorage, $mdDialog) {
 	var vm               = this;
 	vm.goodsName         = '';
 	vm.goodsDescriptions = '';
@@ -34,28 +34,39 @@ function PostController(postService, $scope, $state, auth, AvailableCategory, lo
 					$state.reload();
 				});
 		} else {
-			/**
-			 * First, upload photos and get photo_pathArray,
-			 * then send new post data to backend
-			 */
-			postService
-				.uploadImg(vm.imgEncoded)
-				.then(function(data){
-					postService
-						.sendNewPostInfo({
-							name        : vm.goodsName,
-							description : vm.goodsDescriptions,
-							category    : vm.goodsCategory.label,
-							position_x  : vm.positionX,
-							position_y  : vm.positionY,
-							photo_path  : JSON.stringify(data),
-							owner_uid   : auth.currentUser().uid,
-						})
-						.then(function(data) {
-							logger.success('Your post successes :)', data, 'POST');
-						});
-				});
-			$state.go('root.withSidenav.seek');
+			if(!(vm.positionX && vm.positionY)) {
+				$mdDialog.show(
+					$mdDialog.alert()
+						.parent(angular.element(document.querySelector('#popupContainer')))
+						.clickOutsideToClose(true)
+						.title('未指定位置')
+						.content('請在地圖側標定物品位置.')
+						.ok('Got it!')
+					);
+			} else {
+				/**
+				 * First, upload photos and get photo_pathArray,
+				 * then send new post data to backend
+				 */
+				postService
+					.uploadImg(vm.imgEncoded)
+					.then(function(data){
+						postService
+							.sendNewPostInfo({
+								name        : vm.goodsName,
+								description : vm.goodsDescriptions,
+								category    : vm.goodsCategory.label,
+								position_x  : vm.positionX,
+								position_y  : vm.positionY,
+								photo_path  : JSON.stringify(data),
+								owner_uid   : auth.currentUser().uid,
+							})
+							.then(function(data) {
+								logger.success('Your post successes :)', data, 'POST');
+							});
+					});
+				$state.go('root.withSidenav.seek');
+			}
 		}
 	}
 

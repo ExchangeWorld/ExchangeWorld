11b34a9afaa90e15eff068ff1ebb52ead95aa74f
LonSilent <101703044@nccu.edu.tw>
2015-09-22 12:37:51 +0800

    remove useless js, and some updates

--- app/angular/exchange/exchange.module.js
+++ app/angular/exchange/exchange.module.js
@@ -2,12 +2,10 @@
 
 const angular = require('angular');
 const bulk    = require('bulk-require');
-require('../utils/fill/fill.js');
 
 module.exports = angular.module('app.exchange',
 	[
 		'app.core',
-		'fillHeight',
 	]
 );
 

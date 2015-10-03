259e97a1dc31fe9221dfbd8ac9625f502f426ab3
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-10-04 00:11:06 +0800

    photo viewer style

--- app/angular/core/REST.config.js
+++ app/angular/core/REST.config.js
@@ -4,7 +4,7 @@ const coreModule = require('./core.module');
 coreModule.config(RestConfig);
 
 function RestConfig(RestangularProvider) {
-	// RestangularProvider.setBaseUrl('api');
+	//RestangularProvider.setBaseUrl('api');
 	RestangularProvider.setBaseUrl('http://noel.plsm.cs.nccu.edu.tw:3002/api');
 
 	// set params for multiple methods at once

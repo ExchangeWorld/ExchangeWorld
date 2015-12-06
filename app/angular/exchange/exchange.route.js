1874db06f8e43859ece2d874c46df079dc7ca6f9
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-12-07 01:12:27 +0800

    height

--- app/angular/exchange/exchange.route.js
+++ app/angular/exchange/exchange.route.js
@@ -29,7 +29,7 @@ function getStates() {
 					},
 					/** @ngInject */
 					mapSize: function($window) {
-						return `${parseInt($window.innerWidth*0.25)}x${parseInt($window.innerHeight*0.3)}`;
+						return `${parseInt($window.innerWidth*0.25)}x${parseInt(($window.innerHeight-56)*0.3)}`;
 					}
 				},
 			}

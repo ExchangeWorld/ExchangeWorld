0bd181add8acd466af4117ba6131e28b5470f386
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-12-08 22:59:10 +0800

    中文moment

--- app/angular/core/config.js
+++ app/angular/core/config.js
@@ -2,8 +2,11 @@
 
 const coreModule = require('./core.module');
 const angular    = require('angular');
+const moment   = require('moment');
+
 coreModule.config(toastConfig);
 coreModule.config(facebookprovider);
+coreModule.config(momentLocale);
 
 
 /** @ngInject */
@@ -36,4 +39,9 @@ function facebookprovider(FacebookProvider) {
 	// Set your appId through the setAppId method or use the shortcut in the initialize method directly.
 	// FacebookProvider.init('376506855853722'); // exwd appID
 	FacebookProvider.init('398517123645939'); // exwd Dev_appID
+}
+
+function momentLocale() {
+	require('moment/locale/zh-tw');
+	moment.locale('zh-tw');
 }

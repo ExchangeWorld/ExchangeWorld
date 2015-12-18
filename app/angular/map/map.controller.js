1773dfebae9e1dfeb05e70e3384d78e2bc5bfc03
Xhriman <seal789ie@gmail.com>
2015-12-18 12:17:57 +0800

    fix storage bug

--- app/angular/map/map.controller.js
+++ app/angular/map/map.controller.js
@@ -3,7 +3,8 @@
 
 const mapModule  = require('./map.module');
 const _          = require('lodash');
-var GoodsOverlay = require('./GoodsOverlay.js');
+let GoodsOverlay = require('./GoodsOverlay.js');
+let MarkerOverlay = require('./MarkerOverlay.js');
 
 // Controller name 'MapController' has been used by ng-map
 mapModule.controller('MapCtrl', MapController);
@@ -25,21 +26,23 @@ function MapController(
 	var map     = null;
 	var goods   = [];
 	var overlay = null;
+	var queueData = null;
 	const vm    = this;
 
 	vm.coords = $localStorage.position
 		? $localStorage.position
-		: [25.05517828690749, 121.54136714595292]; // 政大 [24.98918974905472, 121.57591535186772];
+		: [25.05517828690749, 121.54136714595292];
+		// 政大 [24.98918974905472, 121.57591535186772];
 
 	vm.zoom            = 12;
 	vm.draggableCursor = 'default';
 	vm.draggingCursor  = 'default';
 	vm.olcRecord = { lat: vm.coords[0], lng: vm.coords[1] };
-	// vm.mapStyle        = require('./mapStyle.json');
+	vm.mapStyle        = require('./mapStyle.json');
 	// $scope.$on('mapInitialized', mapInitialized);
 	NgMap.getMap().then(mapInitialized);
 
-	// activate();
+	activate();
 
 	/* After map is loaded */
 	function mapInitialized(evtMap) {
@@ -47,20 +50,30 @@ function MapController(
 		vm.findMyLocation = getCurrentPosition;
 		vm.placeChanged   = placeChanged;
 		vm.zoomChanged    = zoomChanged;
+		MarkerOverlay.prototype = new google.maps.OverlayView();
 		GoodsOverlay.prototype = new google.maps.OverlayView();
 
 		$scope.$on('goodsChanged', goodsChanged);
 		$scope.$on('mapMoveTo', mapMoveTo);
 		$scope.$on('openGoodsOverlay', openGoodsOverlay);
 		$scope.$on('closeGoodsOverlay', closeGoodsOverlay);
+		$scope.$on('getBound', getBound);
+		$scope.$on('highlightMarker', highlightMarker);
+		$scope.$on('markGoodViewed', markGoodViewed);
 		$rootScope.$on('$stateChangeSuccess', urlChanged);
+
+		// If event is called before init
+		if (queueData) {
+			goodsChanged(null, queueData);
+		}
+
 		google.maps.event.addListener(map, 'idle', olcChanged);
 		google.maps.event.addListener(map, 'bounds_changed', boundChanged);
 		google.maps.event.addListener(map, 'mousedown', onMousedown);
 		google.maps.event.addListener(map, 'mouseup', onMouseup);
 		google.maps.event.addListener(map, 'dragstart', onDragstart);
 
-		activate();
+		// activate();
 		boundChanged();
 	}
 
@@ -68,11 +81,11 @@ function MapController(
 	function activate() {
 		if ($stateParams.olc) {
 			const coord = OpenLocationCode.decode($stateParams.olc.replace(' ','+'));
-			// vm.coords = [coord.latitudeCenter, coord.longitudeCenter];
-			map.panTo({
-				lat : coord.latitudeCenter,
-				lng : coord.longitudeCenter,
-			});
+			vm.coords = [coord.latitudeCenter, coord.longitudeCenter];
+			// map.panTo({
+				// lat : coord.latitudeCenter,
+				// lng : coord.longitudeCenter,
+			// });
 		// } else if ($stateParams.hasOwnProperty('olc')) {
 		// 	geolocation
 		// 		.getLocation({maximumAge:60000, timeout:5000, enableHighAccuracy:true})
@@ -81,17 +94,23 @@ function MapController(
 		// 		});
 		}
 		if (!isNaN($stateParams.z)) {
-			// vm.zoom = parseInt($stateParams.z, 10);
-			map.setZoom(parseInt($stateParams.z, 10));
+			vm.zoom = parseInt($stateParams.z, 10);
+			// map.setZoom(parseInt($stateParams.z, 10));
 		}
 
 		if ($state.current.title === 'post') {
-			map.setOptions({ 
-				draggableCursor:'crosshair', 
-				draggingCursor: 'crosshair'}
-			);
+			vm.draggableCursor = 'crosshair';
+			vm.draggingCursor  = 'crosshair';
+			// map.setOptions({
+			// 	draggableCursor:'crosshair',
+			// 	draggingCursor: 'crosshair'}
+			// );
 		}
-		vm.smallMap = $state.current.title === 'exchange';
+
+		$scope.$on('goodsChanged', goodsChangedBeforeMap);
+		$scope.$on('mapMoveTo', mapMoveToBeforeMap);
+
+		// vm.smallMap = $state.current.title === 'exchange';
 	}
 
 	function getCurrentPosition() {
@@ -131,13 +150,13 @@ function MapController(
 
 				if (
 					bound.getNorthEast().lat() > 85 &&
-					bound.getSouthWest().lat() < -85 
+					bound.getSouthWest().lat() < -85
 				) {
 					const zoom = map.getZoom();
 					map.setZoom(parseInt(zoom, 10) + 1);
 				} else if (
 					bound.getNorthEast().lat() > 85 ||
-					bound.getSouthWest().lat() < -85 
+					bound.getSouthWest().lat() < -85
 				) {
 					const originCenter = bound.getCenter();
 					const latOffset = bound.getNorthEast().lat() > 85
@@ -158,16 +177,28 @@ function MapController(
 
 	}
 
+	function getBound() {
+		if (map) return map.getBounds();
+
+		return undefined;
+	}
+
+	function goodsChangedBeforeMap(e, data) {
+		queueData = data;
+	}
 	/**
 	 * Receive the goods from seek controller
 	 * Draw maker and overlay here.
 	 */
 	function goodsChanged(e, data) {
+		// console.log(data);
 		closeGoodsOverlay();
 
 		/* 1. Clean unused marker */
 		var hashTable = {};
-		data.forEach(function(obj, index) { hashTable[obj.gid] = index; });
+
+		data.forEach( (obj, i) => hashTable[obj.gid] = i );
+
 		goods
 			.filter(function(good) {
 				if (!(good.gid in hashTable)) return true;
@@ -180,25 +211,32 @@ function MapController(
 				oldGood.marker.setMap(null);
 			});
 
+		let viewedGoods = $localStorage.viewedGoods || [];
+
 		/* 2. Draw new Marker on map */
 		goods = data.map(function(good) {
 			if (good.marker) {
 				return good;
 			}
 
-			var icon = `../../images/mapMarker/${good.category}.png`;
-
-			good.marker = new google.maps.Marker({
-				position: new google.maps.LatLng(good.position_y, good.position_x),
-				map: map,
-				icon: icon
-			});
-
-			/* 3. Click Event that Generate a new overlay which can transistTo state of goods */
-			good.marker.addListener('mouseup', function() {
-				overlay = new GoodsOverlay(map, good, $state, $mdSidenav, closeGoodsOverlay);
-			});
-
+			const isViewed = _.indexOf(viewedGoods, good.gid) > -1;
+			good.marker = new MarkerOverlay(
+				map,
+				good.category,
+				isViewed,  //viewed
+				new google.maps.LatLng(good.position_y, good.position_x), //latlng
+				map.getZoom(), //zoom
+				'rgb(255, 167, 38)',
+				//highColor
+				// 3. Click Event that Generate a new overlay which can transistTo state of goods
+				() => {
+					overlay = new GoodsOverlay(map, good, $state, $mdSidenav, closeGoodsOverlay);
+					if (!isViewed) {
+						viewedGoods.push(good.id);
+						$localStorage.viewedGoods = _.chain(viewedGoods).uniq().sort(n => n);
+					}
+				}
+			);
 			return good;
 		});
 	}
@@ -212,11 +250,11 @@ function MapController(
 			toState.title === 'seek' &&
 			!toParams.olc
 		) {
-			const lat = vm.olcRecord.lat instanceof(Function) 
+			const lat = vm.olcRecord.lat instanceof(Function)
 				? vm.olcRecord.lat()
 				: vm.olcRecord.lat;
 
-			const lng = vm.olcRecord.lng instanceof(Function) 
+			const lng = vm.olcRecord.lng instanceof(Function)
 				? vm.olcRecord.lng()
 				: vm.olcRecord.lng;
 
@@ -248,7 +286,6 @@ function MapController(
 			});
 		}
 
-		// console.log(toState);
 		if (toState.name.indexOf('root.withSidenav.seek') === -1) {
 			vm.olcRecord = map.getCenter();
 		}
@@ -293,6 +330,20 @@ function MapController(
 			location : 'replace',
 			notify : false,
 		});
+	}
+
+	function highlightMarker(e, gid) {
+		_findGood(gid).marker.toggleHighlight();
+	}
+
+	function markGoodViewed(e, gid) {
+		_findGood(gid).marker.setMarkerViewed();
+		const viewedGoods = $localStorage.viewedGoods || [];
+		$localStorage.viewedGoods = _.uniq(viewedGoods.concat(gid));
+	}
+
+	function mapMoveToBeforeMap(e, lat, lng) {
+		vm.coords = [lat, lng];
 	}
 
 	function mapMoveTo(e, lat, lng) {

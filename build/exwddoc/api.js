e421c7827e13d9278bc7a3b6757dbe49d8e50995
noelBao <noeleon930@gmail.com>
2015-12-27 22:13:59 +0800

    20151227

---
+++ build/exwddoc/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Authenticate",
+        "Chatroom",
+        "Comment",
+        "Exchange",
+        "Follow",
+        "Goods",
+        "Message",
+        "Notification",
+        "Queue",
+        "Search",
+        "Star",
+        "Upload",
+        "User"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});

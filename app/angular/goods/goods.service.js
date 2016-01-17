23632cf34a19fef68765d278cccc292570d47214
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2016-01-18 02:29:19 +0800

    trim byuserGen

--- app/angular/goods/goods.service.js
+++ app/angular/goods/goods.service.js
@@ -84,7 +84,6 @@ function goodsService(Restangular, $q, exception, $mdDialog) {
 				goods.description = des;
 				goods.route       = 'goods/edit';
 				goods.photo_path = JSON.stringify(goods.photo_path);
-				goods.byuser = byuserGen(goods.owner_uid);
 
 				goods
 					.put()

335296fc3d541a18a1942adc036a237fb7db36e2
ssarcandy <ssarcandy@gmail.com>
2016-02-20 17:38:49 +0800

    split category to data/category.json

--- app/angular/core/constants.js
+++ app/angular/core/constants.js
@@ -1,40 +1,9 @@
 'use strict';
 
-const coreModule  = require('./core.module');
-const AppSettings = { appTitle : 'ExchangeWorld', };
+const coreModule        = require('./core.module');
+const AvailableCategory = require('../../data/category.json');
+const AppSettings       = { appTitle : 'ExchangeWorld', };
 
-/**
- * define all avalible categories
- */
-const AvailableCategory = [
-	{
-		label: "all",
-		alias: "全部"
-	},
-	{
-		label: "Christmas",
-		alias: "聖誕禮物"
-	},
-	{
-		label: "Books",
-		alias: "書"
-	},
-	{
-		label: "Textbooks",
-		alias: "教科書"
-	},
-	{
-		label: "3C",
-		alias: "電子產品"
-	}, 
-	{
-		label: "Clothes",
-		alias: "服飾"
-	},
-	{
-		label: "Others",
-		alias: "其他"
-	}
-];
+
 coreModule.constant('AppSettings', AppSettings);
 coreModule.constant('AvailableCategory', AvailableCategory);

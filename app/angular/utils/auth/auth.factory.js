ab93bb37ad85a811c4c171229958defed08057a2
ssarcandy <ssarcandy@gmail.com>
2016-02-27 14:46:36 +0800

    fix fb long logic

--- app/angular/utils/auth/auth.factory.js
+++ app/angular/utils/auth/auth.factory.js
@@ -29,7 +29,6 @@ function auth(facebookService, $q, $localStorage, $mdDialog, Restangular, $rootS
 		try {
 			if (fb) {
 				await facebookService.login(); // login to facebook.
-				currentUser = await fetchMe();
 			} else {
 				let token = await Restangular
 					.all('/authenticate/login')
@@ -40,9 +39,9 @@ function auth(facebookService, $q, $localStorage, $mdDialog, Restangular, $rootS
 					});
 
 				updateToken(token.token);
-				$rootScope.isLoggedIn = true;
-				currentUser = await Restangular.oneUrl('user/me').get();
 			}
+			currentUser = await Restangular.oneUrl('user/me').get();
+			$rootScope.isLoggedIn = true;
 			defer.resolve(currentUser);
 		} catch (err) {
 			defer.reject(err);

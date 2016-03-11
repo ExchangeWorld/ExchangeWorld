ff74ff947709fe5b17a42b950d68787b8c88a9bc
ssarcandy <ssarcandy@gmail.com>
2016-03-12 00:48:30 +0800

    set baseURL back

--- app/angular/core/REST.config.js
+++ app/angular/core/REST.config.js
@@ -6,7 +6,7 @@ coreModule.run(RestConfig);
 /** ngInject */
 function RestConfig(Restangular, $localStorage) {
 	//RestangularProvider.setBaseUrl('api');
-	Restangular.setBaseUrl('http://dream.cs.nccu.edu.tw:43002/api');
+	Restangular.setBaseUrl('http://exwd.csie.org:43002/api');
 
 	// token will expire if idle 20min
 	Restangular.setDefaultRequestParams(['get', 'remove', 'post', 'put', 'delete'], {token: $localStorage.token});
